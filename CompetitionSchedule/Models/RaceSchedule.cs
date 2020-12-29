d08aaf4ebc36a7426f94b718e4e0a7a38bd0d32c
SamIge7 <sige@spartaglobal.com>
2020-12-29 17:07:40 +0000

    Race Organiser Initial Commit + Model set up

---
+++ CompetitionSchedule/Models/RaceSchedule.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CompetitionSchedule.Models
+{
+    public class RaceSchedule
+    {
+        public int Id { get; set; }
+        public string EventName { get; set; }
+        public string CompetitionDistances { get; set; }
+        [DataType(DataType.Date)]
+        public DateTime CompetitionDate { get; set; }
+        public string Location { get; set; }
+    }
+}
